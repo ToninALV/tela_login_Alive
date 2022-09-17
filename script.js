8e930f9c88f6668407499fa298b09f63b81654b5
ToninALV <laurentinog.antonio@gmail.com>
2022-09-17 19:42:16 -0300

    v2 pronto

---
+++ script.js
@@ -0,0 +1,7 @@
+function pegardados () {
+    const campoSenha = document.querySelector('#senha')
+    console.log(campoSenha.value);
+    const campoEmail = document.querySelector('#email')
+    console.log(campoEmail.value);
+
+}
